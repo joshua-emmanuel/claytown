f2adf954d97863c5ea7b7415f01d6323a465804a
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-07-08 23:05:07 +0100

    Fix product image import issue

--- src/data/products.js
+++ src/data/products.js
@@ -1,4 +1,21 @@
 import ancientPottery from '../assets/img/ancient-pottery.jpg';
+import stillLifePottery from '../assets/img/still-life-with-plants.jpg';
+import stillLifeVase from '../assets/img/still-life-vase.jpg';
+import shapedVase from '../assets/img/shaped-vase.jpg';
+import vaseSet from '../assets/img/vase-set.jpg';
+import colorfulBowls from '../assets/img/many-colorful-bowls-gray.jpg';
+import visionVase from '../assets/img/vision-vase.jpg';
+import soupBowl from '../assets/img/soup-bowl.jpg';
+import spottedTray from '../assets/img/spotted-tray.jpg';
+import stillLifePottery2 from '../assets/img/still-life-pottery.jpg';
+import grayJug from '../assets/img/gray-jug.jpg';
+import traySet from '../assets/img/tray-set.jpg';
+import monochromeTray from '../assets/img/monochrome-tray.jpg';
+import whiteBowls from '../assets/img/white-bowls.jpg';
+import nativeBowls from '../assets/img/native-bowls.jpg';
+import stillLifePottery3 from '../assets/img/still-life-with-plants-2.jpg';
+import specialVases from '../assets/img/special-vases.jpg';
+import threeDTray from '../assets/img/3d-tray.jpg';
 
 const products = [
   {
@@ -9,87 +26,87 @@ const products = [
   {
     name: 'Still life pottery',
     price: 15000,
-    img: '/src/assets/img/still-life-with-plants.jpg',
+    img: stillLifePottery,
   },
   {
     name: 'Still life vase',
     price: 20000,
-    img: '/src/assets/img/still-life-vase.jpg',
+    img: stillLifeVase,
   },
   {
     name: 'Shaped vase',
     price: 12000,
-    img: '/src/assets/img/shaped-vase.jpg',
+    img: shapedVase,
   },
   {
     name: 'Vase set',
     price: 15000,
-    img: '/src/assets/img/vase-set.jpg',
+    img: vaseSet,
   },
   {
     name: 'Colorful',
     price: 9000,
-    img: '/src/assets/img/many-colorful-bowls-gray.jpg',
+    img: colorfulBowls,
   },
   {
     name: 'Vision vase',
     price: 5000,
-    img: '/src/assets/img/many-colorful-bowls-gray.jpg',
+    img: visionVase,
   },
   {
     name: 'Still life pottery',
     price: 15000,
-    img: '/src/assets/img/still-life-pottery.jpg',
+    img: stillLifePottery2,
   },
   {
     name: 'Gray jug',
     price: 3000,
-    img: '/src/assets/img/gray-jug.jpg',
+    img: grayJug,
   },
   {
     name: 'Tray set',
     price: 6000,
-    img: '/src/assets/img/tray-set.jpg',
+    img: traySet,
   },
   {
     name: 'Monochrome tray',
     price: 8000,
-    img: '/src/assets/img/monochrome-tray.jpg',
+    img: monochromeTray,
   },
   {
     name: 'White',
     price: 15000,
-    img: '/src/assets/img/white-bowls.jpg',
+    img: whiteBowls,
   },
   {
     name: 'Native',
     price: 15000,
-    img: '/src/assets/img/native-bowls.jpg',
+    img: nativeBowls,
   },
   {
     name: 'Still life pottery',
     price: 15000,
-    img: '/src/assets/img/still-life-with-plants-2.jpg',
+    img: stillLifePottery3,
   },
   {
     name: 'Special',
     price: 12000,
-    img: '/src/assets/img/special-vases.jpg',
+    img: specialVases,
   },
   {
     name: '3D tray',
     price: 4000,
-    img: '/src/assets/img/3d-tray.jpg',
+    img: threeDTray,
   },
   {
     name: 'Spotted tray',
     price: 3000,
-    img: '/src/assets/img/spotted-tray.jpg',
+    img: spottedTray,
   },
   {
     name: 'Soup bowl',
     price: 1600,
-    img: '/src/assets/img/soup-bowl.jpg',
+    img: soupBowl,
   },
 ];
 
