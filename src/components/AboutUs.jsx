d187acedc9c6271f45a761788e141842b883a1ba
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-07-08 09:18:30 +0100

    Initial commit

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,32 @@
+import aboutUsImg from '../assets/img/about-us-img.png';
+import '../assets/css/AboutUs.css';
+
+function AboutUs() {
+  return (
+    <section className="about-us">
+      <div className="wrapper">
+        <h2 className="font-condensed">About us</h2>
+        <div className="about-us__content">
+          <div>
+            <p>
+              Claytown is where tradition meets modern craftsmanship. We are
+              passionate artisans dedicated to bringing you the finest handmade
+              clay and wooden products from around the world
+            </p>
+          </div>
+          <img src={aboutUsImg} alt="Flower Vases" />
+          <div>
+            <p>
+              Our curated collection celebrates the artistry and cultural
+              heritage of skilled craftsmen, ensuring every piece tells a unique
+              story.
+            </p>
+            <button>Read more</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutUs;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import HomeHero from '../components/HomeHero';
+import AboutUs from '../components/AboutUs';
+import '../assets/css/Home.css';
+
+function Home() {
+  return (
+    <>
+      <HomeHero />
+      <AboutUs />
+    </>
+  );
+}
+
+export default Home;
