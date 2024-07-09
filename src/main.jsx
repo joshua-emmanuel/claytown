b40ef54c419ee279e34a9c0a3aaa53435d500d22
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-07-09 21:38:42 +0100

    Nest whole app in cart context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CartContext } from './context/CartContext.jsx';
 import App from './App.jsx';
 import './assets/css/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CartContext>
+      <App />
+    </CartContext>
   </React.StrictMode>
 );
