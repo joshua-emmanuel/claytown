d6e3afd8253424260bed497a440b7a145f27d76c
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-07-09 21:37:46 +0100

    Update router links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { useReducer } from 'react';
+// import { CartContext, CartDispatchContext } from './context/CartContext';
 import Products from './pages/Products';
 import Cart from './pages/Cart';
 
@@ -8,12 +10,46 @@ const router = createBrowserRouter([
     element: <Products />,
   },
   {
+    path: '/products',
+    element: <Products />,
+  },
+  {
     path: '/cart',
     element: <Cart />,
   },
 ]);
 
+// const initialCart = [];
+
+// function cartReducer(cart, action) {
+//   switch (action) {
+//     case 'add': {
+//       return [
+//         ...cart,
+//         {
+//           id: action.id,
+//           name: action.name,
+//           price: action.price,
+//           img: action.img,
+//         },
+//       ];
+//     }
+//   }
+// }
+
 function App() {
+  // const [cart, dispatch] = useReducer(cartReducer, initialCart);
+
+  // const addToCart = (product) => {
+  //   dispatch({
+  //     type: 'add',
+  //     id: product.id,
+  //     name: product.name,
+  //     price: product.price,
+  //     img: product.img,
+  //   });
+  // };
+
   return <RouterProvider router={router} />;
 }
 
