33fc94d52ef2ea4ce3af9c187ff5c3c20c2554af
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-07-09 21:40:43 +0100

    Add cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,35 @@
+import { useCartContext } from '../context/CartContext';
 import NavBar from '../components/NavBar';
+import prevIcon from '../assets/img/previous-icon.svg';
+import products from '../data/products';
+import CartProduct from '../components/CartProduct';
+import '../assets/css/Cart.css';
 
 function Cart() {
-  return <NavBar />;
+  const { cart } = useCartContext();
+
+  return (
+    <>
+      <NavBar />
+      <main>
+        <div className="wrapper">
+          <div className="cart">
+            <div className="cart__header">
+              <img src={prevIcon} alt="previous icon" />
+              <p className="font-condensed">
+                Cart(<span>{cart.length}</span>)
+              </p>
+            </div>
+            <div className="cart__list">
+              {cart.map((cartItem) => (
+                <CartProduct key={cartItem.id} product={cartItem} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default Cart;
