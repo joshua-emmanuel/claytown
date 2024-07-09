004283fe4b33c05426626cab9df63cb46b08c80f
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-07-09 07:47:39 +0100

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import '../assets/css/Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>ClayTown © 2024. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
