e91f5ce36d0adbf04cbd8d705c9311e86973fca0
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-07-09 23:11:58 +0100

    Add cart summary

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,12 @@
 import { useCartContext } from '../context/CartContext';
+import { Link } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import prevIcon from '../assets/img/previous-icon.svg';
 import CartProduct from '../components/CartProduct';
 import '../assets/css/Cart.css';
 
 function Cart() {
-  const { cart } = useCartContext();
+  const { cart, totalPrice } = useCartContext();
 
   return (
     <>
@@ -14,7 +15,9 @@ function Cart() {
         <div className="wrapper">
           <div className="cart">
             <div className="cart__header">
-              <img src={prevIcon} alt="previous icon" />
+              <Link to="/products">
+                <img src={prevIcon} alt="previous icon" />
+              </Link>
               <p className="font-condensed">
                 Cart(<span>{cart.length}</span>)
               </p>
@@ -23,6 +26,19 @@ function Cart() {
               {cart.map((cartItem) => (
                 <CartProduct key={cartItem.id} product={cartItem} />
               ))}
+            </div>
+            <div className="cart__summary--mobile">
+              <div>
+                <p>Total amount - NGN {totalPrice}</p>
+                <p>Shipping fee - NGN 700</p>
+                <p>Total - NGN {totalPrice + 700}</p>
+              </div>
+              <button className="checkout-button">Checkout</button>
+              <div>
+                <Link className="homepage-link" to="/">
+                  Back to home
+                </Link>
+              </div>
             </div>
           </div>
         </div>
