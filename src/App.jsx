76323c9771de782941459adcf5915eb9c0b2fb00
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-07-09 23:55:11 +0100

    Remove commented code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import { useReducer } from 'react';
-// import { CartContext, CartDispatchContext } from './context/CartContext';
 import Products from './pages/Products';
 import Cart from './pages/Cart';
 
@@ -19,37 +17,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-// const initialCart = [];
-
-// function cartReducer(cart, action) {
-//   switch (action) {
-//     case 'add': {
-//       return [
-//         ...cart,
-//         {
-//           id: action.id,
-//           name: action.name,
-//           price: action.price,
-//           img: action.img,
-//         },
-//       ];
-//     }
-//   }
-// }
-
 function App() {
-  // const [cart, dispatch] = useReducer(cartReducer, initialCart);
-
-  // const addToCart = (product) => {
-  //   dispatch({
-  //     type: 'add',
-  //     id: product.id,
-  //     name: product.name,
-  //     price: product.price,
-  //     img: product.img,
-  //   });
-  // };
-
   return <RouterProvider router={router} />;
 }
 
