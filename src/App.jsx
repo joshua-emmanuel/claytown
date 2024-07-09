fff30a0f5f00a11bb19882bfa7856325dd4d4451
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-07-09 07:50:08 +0100

    Create routes for cart page and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-// import Home from './pages/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Products from './pages/Products';
+import Cart from './pages/Cart';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Products />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+]);
 
 function App() {
-  return <Products />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
