d6ff0ddec8bfe9495ea83f9f2f8e20125ab05dc7
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-07-13 02:35:36 +0100

    Fetch products from timbu api

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,46 @@
+import { useEffect, useState } from 'react';
 import NavBar from '../components/NavBar';
 import '../assets/css/Products.css';
 import Product from '../components/Product';
-import products from '../data/products';
 import Footer from '../components/Footer';
+import LoadingStatus from '../components/LoadingStatus';
 
 function Products() {
+  const [products, setProducts] = useState([]);
+  const [status, setStatus] = useState('loading');
+
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const ORG_ID = import.meta.env.VITE_ORG_ID;
+  const APP_ID = import.meta.env.VITE_APP_ID;
+
+  useEffect(() => {
+    fetch(
+      `/api/products?organization_id=${ORG_ID}&Appid=${APP_ID}&Apikey=${API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const items = data.items;
+        const products = items.map((item) => {
+          return {
+            id: item.id,
+            name: item.name,
+            price: item.current_price[0].NGN[0],
+            description: item.description,
+            img: `https://api.timbu.cloud/images/${item.photos[0].url}`,
+          };
+        });
+        setProducts(products);
+        setStatus('done');
+      })
+      .catch(() => {
+        setStatus('error');
+      });
+  }, []);
+
+  const isLoading = status === 'loading';
+  const isError = status === 'error';
+  const isDone = status === 'done';
+
   return (
     <>
       <NavBar />
@@ -12,9 +48,12 @@ function Products() {
         <div className="wrapper">
           <h1 className="font-condensed">Products</h1>
           <div className="products__list">
-            {products.map((product) => (
-              <Product key={product.id} product={product} />
-            ))}
+            {isLoading && <LoadingStatus>Loading products...</LoadingStatus>}
+            {isError && <LoadingStatus>Oops! An error occured</LoadingStatus>}
+            {isDone &&
+              products.map((product) => (
+                <Product key={product.id} product={product} />
+              ))}
           </div>
         </div>
       </main>
